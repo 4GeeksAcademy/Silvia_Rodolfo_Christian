35b8b14e1df9b7928d48578032ce79974e4ef12f
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-09-04 19:41:16 +0000

    Segundo commit

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<div class="footer container-fluid">
 		<div class="row px-5 d-flex">
-			<!-- Sección de los nombres y redes sociales -->
+			// Sección de los nombres y redes sociales
 			<div class="col-3 d-flex flex-column justify-content-center order-1">
 				<h6><strong>Nosotros</strong></h6>
 				<ul class="list-unstyled">
@@ -42,7 +42,7 @@ export const Footer = () => (
 			// Sección del logo
 			<div class="col-9 d-flex justify-content-end align-items-center order-2">
 				<div class="footer-logo">
-					<img src="Logo sin fondo.png" alt="ASINCRO Logo" class="img-fluid" style="max-width: 150px;">
+					<img src="Logo sin fondo.png" alt="ASINCRO Logo" class="img-fluid" style="max-width: 150px;"/>
 				</div>
 			</div>
 		</div>
