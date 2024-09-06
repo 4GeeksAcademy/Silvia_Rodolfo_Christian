c85a615e9c22bfd53607b2ec9c018f37f7c82f28
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-09-06 16:38:53 +0000

    Tercer commit revisable

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,47 +2,47 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<div class="footer container-fluid">
-		<div class="row px-5 d-flex">
+		<div className="footer container-fluid">
+		<div className="row px-5 d-flex">
 			// Sección de los nombres y redes sociales
-			<div class="col-3 d-flex flex-column justify-content-center order-1">
+			<div className="col-3 d-flex flex-column justify-content-center order-1">
 				<h6><strong>Nosotros</strong></h6>
-				<ul class="list-unstyled">
-					<li class="d-flex align-items-center">
-						<span class="name">Antonio Buigues García</span> 
-						<span class="social-icons d-flex">
-							<a href="#"><i class="fa-brands fa-github"></i></a>
-							<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+				<ul className="list-unstyled">
+					<li className="d-flex align-items-center">
+						<span className="name">Antonio Buigues García</span> 
+						<span className="social-icons d-flex">
+							<a href="#"><i className="fa-brands fa-github"></i></a>
+							<a href="#"><i className="fa-brands fa-linkedin"></i></a>
 						</span>
 					</li>
-					<li class="d-flex align-items-center">
-						<span class="name">Christian David Villar Colodro</span>
-						<span class="social-icons d-flex">
-							<a href="#"><i class="fa-brands fa-github"></i></a>
-							<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+					<li className="d-flex align-items-center">
+						<span className="name">Christian David Villar Colodro</span>
+						<span className="social-icons d-flex">
+							<a href="#"><i className="fa-brands fa-github"></i></a>
+							<a href="#"><i className="fa-brands fa-linkedin"></i></a>
 						</span>
 					</li>
-					<li class="d-flex align-items-center">
-						<span class="name">Rodolfo D’alessandro</span>
-						<span class="social-icons d-flex">
-							<a href="#"><i class="fa-brands fa-github"></i></a>
-							<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+					<li className="d-flex align-items-center">
+						<span className="name">Rodolfo D’alessandro</span>
+						<span className="social-icons d-flex">
+							<a href="#"><i className="fa-brands fa-github"></i></a>
+							<a href="#"><i className="fa-brands fa-linkedin"></i></a>
 						</span>
 					</li>
-					<li class="d-flex align-items-center">
-						<span class="name">Silvia González Moraga</span>
-						<span class="social-icons d-flex">
-							<a href="#"><i class="fa-brands fa-github"></i></a>
-							<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+					<li className="d-flex align-items-center">
+						<span className="name">Silvia González Moraga</span>
+						<span className="social-icons d-flex">
+							<a href="#"><i className="fa-brands fa-github"></i></a>
+							<a href="#"><i className="fa-brands fa-linkedin"></i></a>
 						</span>
 					</li>
 				</ul>
 			</div>
 			
 			// Sección del logo
-			<div class="col-9 d-flex justify-content-end align-items-center order-2">
-				<div class="footer-logo">
-					<img src="Logo sin fondo.png" alt="ASINCRO Logo" class="img-fluid" style="max-width: 150px;"/>
+			<div className="col-9 d-flex justify-content-end align-items-center order-2">
+				<div className="footer-logo">
+					<img src="Logo sin fondo.png" alt="ASINCRO Logo" className="img-fluid" style="max-width: 150px;"/>
 				</div>
 			</div>
 		</div>
