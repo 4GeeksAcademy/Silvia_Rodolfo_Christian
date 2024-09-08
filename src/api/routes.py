f3267c7be33ac29a578897def9b20128b9115e48
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-09-08 09:16:09 +0000

    [Update Modes y app.py]

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,38 +20,3 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
-# @app.route('/form', methods=['POST'])
-# def create_form():
-#     try:
-#         # Extraer datos del cuerpo de la solicitud
-#         data = request.get_json()
-
-#         # Crear la instancia de Form
-#         new_form = Form(
-#             initialDate=data['initialDate'],
-#             finalDate=data['finalDate'],
-#             userId=data['userId']
-#         )
-#         db.session.add(new_form)
-#         db.session.commit()  # Necesario para generar el id del form antes de añadir los detalles
-
-#         # Agregar los DetailForm
-#         for detail in data.get('details', []):
-#             new_detail = DetailForm(
-#                 formId=new_form.id,
-#                 stockId=detail['stockId'],
-#                 description=detail['description'],
-#                 quantity=detail['quantity'],
-#                 type=StockTypeEnum(detail['type'])  # Usamos el Enum para asignar el tipo
-#             )
-#             db.session.add(new_detail)
-
-#         # Confirmar la transacción para los DetailForm
-#         db.session.commit()
-
-#         # Devolver la respuesta con los datos del Form creado
-#         return jsonify({
-#             "message": "Form and details created successfully",
-#             "form": new_form.serialize(),
-#             "details": [detail.serialize() for detail in new_form.form_relationship]  # Devolver los detalles del form
-#         }), 201
