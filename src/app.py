36d83b06603dd98066c3788f6662861041dd1615
Elroro23 <rodolfodalessandro23@gmail.com>
2024-09-09 18:59:53 +0000

    Traigo modificaciones del archivo models a mi rama.

--- src/app.py
+++ src/app.py
@@ -7,13 +7,12 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db, User, UserTypeEnum, Stock,StockTypeEnum,Form,DetailForm
-from api.models import db, User, UserTypeEnum, Stock, StockTypeEnum, Form, DetailForm
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token #Importamos las librerias necesarias.
+from flask_jwt_extended import create_access_token #Importamos las librerias necesarias del token.
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
