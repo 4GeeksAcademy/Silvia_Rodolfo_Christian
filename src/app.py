2936849c3b5b31e21db78a63c8a42cb5785e236d
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-09-09 18:59:13 +0000

    [Update Models]

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db, User, UserTypeEnum, Stock,StockTypeEnum,Form,DetailForm
-from api.models import db, User, UserTypeEnum, Stock, StockTypeEnum, Form, DetailForm
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
