303bb0cdeedadc69bb9b950b6db7277a97e75d82
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-09-27 11:52:20 +0000

    [Migrations]

--- migrations/versions/33b71fe7c62c_.py
+++ migrations/versions/33b71fe7c62c_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 503902e28579
+Revision ID: 33b71fe7c62c
 Revises: 
-Create Date: 2024-09-26 23:07:32.363519
+Create Date: 2024-09-27 11:52:08.080112
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '503902e28579'
+revision = '33b71fe7c62c'
 down_revision = None
 branch_labels = None
 depends_on = None
